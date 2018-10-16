6ca2fe54d2ebe9468b91cda320f0868866ec372d
Max <maxliao@max-Ubuntu16.04.3>
2018-10-16 17:24:27 +0800

    1. show the single query result 2. show query all result

--- vms_query.php
+++ vms_query.php
@@ -20,7 +20,6 @@ $js = '<script>
 		})
 	} );
 	</script>' ;
-		#$( "#datepicker" ).datepicker();
 print_page_start('VMS 統計', $js) ;
 
 vms_checkLogin() ;
@@ -29,12 +28,15 @@ vms_menu_page() ;
 
 
 query_single_form($conn) ; //單個義工查詢
+show_single_query($conn) ;
+
 query_all_form($conn) ; //全體查詢
+show_all_query($conn) ;
 
 
 function query_single_form($con)
 {
-	echo "<form action=\"vms_do_single_query.php\" method=\"post\">" ;
+	echo "<form action=\"vms_query.php\" method=\"post\">" ;
 	echo "<center>\n" ;
 	echo "<br><br><hr>\n<h2>單人義工參與工作查詢</h2><br>\n" ;
 	echo "<table border=\"2\" style=\"width: 800px\">\n" ;
@@ -54,9 +56,41 @@ function query_single_form($con)
 	echo "</center></form>\n" ;
 }
 
+function show_single_query($con)
+{
+	$button = $_POST['qsingle'] ;
+	$volun_man_name = $_POST['volun_man_name'] ;
+	$sdate = $_POST['s_date'] ;
+	$edate = $_POST['e_date'] ;
+
+	if($button == null || $volun_man_name == null || $sdate == null || $edate == null)
+		return ;
+
+
+	$sql = "SELECT * FROM volun_atten WHERE date BETWEEN '$sdate' AND '$edate' AND volun_man_name = '$volun_man_name'" ;
+	//echo $sql ;
+	//exit;
+	if(!$result = $con->query($sql))
+	{
+		echo 'query failed' ;
+	} else {
+		if($result->num_rows > 0)
+		{
+		    echo "<center>\n" ;
+			echo '<br><br><hr><h3>義工：' . $volun_man_name . '於&nbsp;&nbsp;' . $sdate . '&nbsp;&nbsp;&nbsp;~&nbsp;&nbsp;&nbsp;' . $edate . '&nbsp;&nbsp;共有&nbsp;' . $result->num_rows . '&nbsp;筆參與工作紀錄</h3><br>' ;
+			echo "</center>\n" ;
+			#while ($row = $result->fetch_assoc()) {	
+		} else {
+		    echo "<center>\n" ;
+			echo '<br><br><hr><h3>義工：' . $volun_man_name . '於&nbsp;&nbsp;' . $sdate . '&nbsp;&nbsp;&nbsp;~&nbsp;&nbsp;&nbsp;' . $edate . '&nbsp;&nbsp;間無工作紀錄</h3><br>' ;
+			echo "</center>\n" ;
+		}
+	}
+}
+
 function query_all_form($con)
 {
-	echo "<form action=\"vms_do_single_query.php\" method=\"post\">" ;
+	echo "<form action=\"vms_query.php\" method=\"post\">" ;
 	echo "<center>\n" ;
 	echo "<br><br><hr>\n<h2>全體義工參與工作查詢</h2><br>\n" ;
 	echo "<table border=\"2\" style=\"width: 800px\">\n" ;
@@ -68,8 +102,39 @@ function query_all_form($con)
 
 	echo "</tr>\n" ;
 	echo "</table>\n" ;
-	echo "<br><br><input type=\"submit\" name=\"qsingle\" value=\"查詢\">\n" ;
+	echo "<br><br><input type=\"submit\" name=\"qall\" value=\"查詢\">\n" ;
 	echo "</center></form>\n" ;
+}
+
+function show_all_query($con)
+{
+	$button = $_POST['qall'] ;
+	$sdate = $_POST['s_date'] ;
+	$edate = $_POST['e_date'] ;
+
+	if($button == null || $sdate == null || $edate == null)
+		return ;
+
+
+	$sql = "SELECT * FROM volun_atten WHERE date BETWEEN '$sdate' AND '$edate'" ;
+	//echo $sql ;
+	//exit;
+	if(!$result = $con->query($sql))
+	{
+		echo 'query failed' ;
+	} else {
+		if($result->num_rows > 0)
+		{
+		    echo "<center>\n" ;
+			echo '<br><br><hr><h3>於&nbsp;&nbsp;' . $sdate . '&nbsp;&nbsp;&nbsp;~&nbsp;&nbsp;&nbsp;' . $edate . '&nbsp;&nbsp;共有&nbsp;' . $result->num_rows . '&nbsp;筆工作紀錄</h3><br>' ;
+			#echo "<table border=\"2\" style=\"width: 800px\">\n" ;
+			#while ($row = $result->fetch_assoc()) {	
+				#echo '<br>義工：' . $volun_man_name . '於&nbsp;&nbsp;' . $sdate . '&nbsp;&nbsp;&nbsp;~&nbsp;&nbsp;&nbsp;' . $edate . '&nbsp;&nbsp;共有&nbsp;' . $result->num_rows . '&nbsp;筆參與工作紀錄<br>' ;
+			#}
+		} else {
+			echo '<br>於&nbsp;&nbsp;' . $sdate . '&nbsp;&nbsp;&nbsp;~&nbsp;&nbsp;&nbsp;' . $edate . '&nbsp;&nbsp;間無紀錄' ;
+		}
+	}
 }
 
 
